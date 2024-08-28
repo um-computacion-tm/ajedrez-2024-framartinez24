032598abd6a96a3ad132392f54608b8a478a2005
Fran <fae.martinez@alumno.um.edu.ar>
2024-08-27 21:35:18 -0300

    CircleCI test purpose

---
+++ game/test_chess.py
@@ -0,0 +1,33 @@
+import unittest
+from board import Board
+from chess import Chess
+
+class ChessTest(unittest.TestCase):
+    def setUp(self):
+        self.chess = Chess()
+
+    def test_init(self):
+        self.assertIsInstance(self.chess.__board__, Board)  
+        self.assertEqual(self.chess.turn, "WHITE")
+
+    def test_is_playing(self):
+        self.assertTrue(self.chess.is_playing(),True)
+
+    def test_move(self):
+        self.chess.move(0, 0, 0, 1)
+        piece = self.chess.__board__.get_piece(0, 1)
+        self.assertIsNotNone(piece)
+        self.assertEqual(self.chess.turn, "BLACK")
+
+    def test_show_board(self):
+        board_str = self.chess.show_board()
+        self.assertIsInstance(board_str, str)
+
+    def test_change_turn(self):
+        self.chess.change_turn()
+        self.assertEqual(self.chess.turn, "BLACK")
+        self.chess.change_turn()
+        self.assertEqual(self.chess.turn, "WHITE")
+
+if __name__ == '__main__':
+    unittest.main()
