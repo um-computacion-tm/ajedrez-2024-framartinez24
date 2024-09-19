6bf4a117538be577696c47bcf584b02d981f7699
Fran <fae.martinez@alumno.um.edu.ar>
2024-09-19 14:53:26 -0300

    cli_test

--- test/test_cli.py
+++ test/test_cli.py
@@ -5,29 +5,29 @@ from cli import play
 
 
 class TestCli(unittest.TestCase):
-    @patch(  # este patch controla lo que hace el input
+    @patch( 
         'builtins.input',
-        side_effect=['1', '1', '2', '2'], # estos son los valores que simula lo que ingresaria el usuario
+        side_effect=['1', '1', '2', '2'], 
     )
-    @patch('builtins.print') # este patch controla lo que hace el print
+    @patch('builtins.print') 
     @patch.object(Chess, 'move')
     def test_happy_path(
         self,
         mock_chess_move,
         mock_print,
         mock_input,
-    ): #
+    ): 
         chess = Chess()
         play(chess)
         self.assertEqual(mock_input.call_count, 4)
         self.assertEqual(mock_print.call_count, 2)
         self.assertEqual(mock_chess_move.call_count, 1)
 
-    @patch(  # este patch controla lo que hace el input
+    @patch( 
         'builtins.input',
-        side_effect=['hola', '1', '2', '2'], # estos son los valores que simula lo que ingresaria el usuario
+        side_effect=['hola', '1', '2', '2'], 
     )
-    @patch('builtins.print') # este patch controla lo que hace el print
+    @patch('builtins.print') 
     @patch.object(Chess, 'move')
     def test_not_happy_path(
         self,
@@ -41,11 +41,11 @@ class TestCli(unittest.TestCase):
         self.assertEqual(mock_print.call_count, 3)
         self.assertEqual(mock_chess_move.call_count, 0)
 
-    @patch(  # este patch controla lo que hace el input
+    @patch(  
         'builtins.input',
-        side_effect=['1', '1', '2', 'hola'], # estos son los valores que simula lo que ingresaria el usuario
+        side_effect=['1', '1', '2', 'hola'], 
     )
-    @patch('builtins.print') # este patch controla lo que hace el print
+    @patch('builtins.print') # 
     @patch.object(Chess, 'move')
     def test_more_not_happy_path(
         self,
@@ -59,24 +59,3 @@ class TestCli(unittest.TestCase):
         self.assertEqual(mock_print.call_count, 3)
         self.assertEqual(mock_chess_move.call_count, 0)
 
-    # @patch(  # este patch controla lo que hace el input
-    #     'builtins.input',
-    #     side_effect=['1', '1', '2', '1'], # estos son los valores que simula lo que ingresaria el usuario
-    # )
-    # @patch('builtins.print') # este patch controla lo que hace el print
-    # @patch.object(
-    #     Chess,
-    #     'move',
-    #     side_effect=InvalidMove(),
-    # )
-    # def test_invalid_move(
-    #     self,
-    #     mock_chess_move,
-    #     mock_print,
-    #     mock_input,
-    # ): #
-    #     chess = Chess()
-    #     play(chess)
-    #     self.assertEqual(mock_input.call_count, 4)
-    #     self.assertEqual(mock_print.call_count, 2)
-    #     self.assertEqual(mock_chess_move.call_count, 1)
